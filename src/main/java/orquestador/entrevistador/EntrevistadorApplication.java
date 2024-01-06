de5750f1822956c3136dbb37a2a3dcc10010fe24
JorgeI246 <82060766+Jorgeivan246@users.noreply.github.com>
2024-01-06 10:47:05 -0500

    Primera version de la estructura del orquestador

---
+++ src/main/java/orquestador/entrevistador/EntrevistadorApplication.java
@@ -0,0 +1,13 @@
+package orquestador.entrevistador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orquestador/entrevistador/EntrevistadorApplicationTests.java
@@ -0,0 +1,13 @@
+package orquestador.entrevistador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
