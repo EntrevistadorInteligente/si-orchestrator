cda764511b0975d00373f4413e6d2b9606d50cea
Kirenai <pool_dota2@hotmail.com>
2024-01-21 14:29:40 -0500

    Se modifica inyeccion por argumento al bean requerido

--- src/main/java/com/entrevistador/orquestador/infrastructure/beanconfiguration/ServicesBeanConfiguration.java
+++ src/main/java/com/entrevistador/orquestador/infrastructure/beanconfiguration/ServicesBeanConfiguration.java
@@ -10,52 +10,50 @@ import com.entrevistador.orquestador.dominio.service.SolicitudPreparacionEntrevi
 import com.entrevistador.orquestador.dominio.service.ValidadorEventosSimultaneosService;
 import com.entrevistador.orquestador.dominio.service.ValidadorPdfService;
 import com.entrevistador.orquestador.infrastructure.adapter.repository.ProcesoEntrevistaRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class ServicesBeanConfiguration {
 
-    private final ProcesoEntrevistaRepository procesoEntrevistaRepository;
-
     @Bean
-    public ActualizarEstadoProvesoEntrevistaService actualizarEstadoProvesoEntrevistaService(){
+    public ActualizarEstadoProvesoEntrevistaService actualizarEstadoProvesoEntrevistaService(ProcesoEntrevistaRepository procesoEntrevistaRepository) {
         return new ActualizarEstadoProvesoEntrevistaService(procesoEntrevistaRepository);
     }
+
     @Bean
-    public ActualizarInformacionEntrevistaService actualizarInformacionEntrevistaService(){
+    public ActualizarInformacionEntrevistaService actualizarInformacionEntrevistaService() {
         return new ActualizarInformacionEntrevistaService();
     }
 
     @Bean
-    public CrearEntrevistaAlternativaService crearEntrevistaAlternativaService(NotificarFrontEntrevistaFallidaService notificarFrontEntrevistaFallidaService){
+    public CrearEntrevistaAlternativaService crearEntrevistaAlternativaService(NotificarFrontEntrevistaFallidaService notificarFrontEntrevistaFallidaService,
+                                                                               ProcesoEntrevistaRepository procesoEntrevistaRepository) {
         return new CrearEntrevistaAlternativaService(notificarFrontEntrevistaFallidaService, procesoEntrevistaRepository);
     }
 
     @Bean
-    public CrearEntrevistaService crearEntrevistaService(){
+    public CrearEntrevistaService crearEntrevistaService() {
         return new CrearEntrevistaService();
     }
 
     @Bean
-    public NotificarFrontEntrevistaFallidaService notificarFrontEntrevistaFallidaService(){
+    public NotificarFrontEntrevistaFallidaService notificarFrontEntrevistaFallidaService() {
         return new NotificarFrontEntrevistaFallidaService();
     }
 
     @Bean
-    public SolicitudPreparacionEntrevistaService solicitudPreparacionEntrevistaService(){
+    public SolicitudPreparacionEntrevistaService solicitudPreparacionEntrevistaService() {
         return new SolicitudPreparacionEntrevistaService();
     }
 
     @Bean
-    public ValidadorEventosSimultaneosService validadorEventosSimultaneosService(){
+    public ValidadorEventosSimultaneosService validadorEventosSimultaneosService() {
         return new ValidadorEventosSimultaneosService();
     }
 
     @Bean
-    public ValidadorPdfService validadorPdfService(){
+    public ValidadorPdfService validadorPdfService() {
         return new ValidadorPdfService();
     }
 
