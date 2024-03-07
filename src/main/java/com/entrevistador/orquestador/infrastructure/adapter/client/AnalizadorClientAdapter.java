b58fc3065690d2ac08889f5565de9ea0c26554ab
Kirenai <pool_dota2@hotmail.com>
2024-03-06 22:21:14 -0500

    Mejorando la creacion de WebClient para cada adaptador

--- src/main/java/com/entrevistador/orquestador/infrastructure/adapter/client/AnalizadorClientAdapter.java
+++ src/main/java/com/entrevistador/orquestador/infrastructure/adapter/client/AnalizadorClientAdapter.java
@@ -2,23 +2,26 @@ package com.entrevistador.orquestador.infrastructure.adapter.client;
 
 import com.entrevistador.orquestador.dominio.model.dto.PreparacionEntrevistaDto;
 import com.entrevistador.orquestador.dominio.port.client.AnalizadorClient;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
 public class AnalizadorClientAdapter implements AnalizadorClient {
 
     private final WebClient webClient;
+
+    public AnalizadorClientAdapter(@Qualifier("webClientAnalizador") WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public Mono<Void> enviarHojaDeVida(PreparacionEntrevistaDto preparacionEntrevistaDto) {
         return this.webClient
                 .post()
-                .uri("URI")
+                .uri("/api")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(preparacionEntrevistaDto), PreparacionEntrevistaDto.class)
                 .retrieve()
--- src/main/java/com/entrevistador/orquestador/infrastructure/adapter/client/RecopiladorEmpresaClientAdapter.java
+++ src/main/java/com/entrevistador/orquestador/infrastructure/adapter/client/RecopiladorEmpresaClientAdapter.java
@@ -2,23 +2,26 @@ package com.entrevistador.orquestador.infrastructure.adapter.client;
 
 import com.entrevistador.orquestador.dominio.model.dto.FormularioDto;
 import com.entrevistador.orquestador.dominio.port.client.RecopiladorEmpresaClient;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
 public class RecopiladorEmpresaClientAdapter implements RecopiladorEmpresaClient {
 
     private final WebClient webClient;
+
+    public RecopiladorEmpresaClientAdapter(@Qualifier("webClientRecopilador") WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public Mono<Void> enviarInformacionEmpresa(FormularioDto formulario) {
         return this.webClient
                 .post()
-                .uri("URI")
+                .uri("/api")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(formulario), FormularioDto.class)
                 .retrieve()
