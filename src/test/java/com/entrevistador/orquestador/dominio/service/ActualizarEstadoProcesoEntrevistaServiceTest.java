62a3e7c867a67546099faa57e9ef5ad5648699de
Kirenai <pool_dota2@hotmail.com>
2024-03-31 04:38:14 -0500

    Agregando test para ActualizarEstadoProcesoEntrevistaService

--- src/test/java/com/entrevistador/orquestador/dominio/service/ActualizarEstadoProcesoEntrevistaServiceTest.java
+++ src/test/java/com/entrevistador/orquestador/dominio/service/ActualizarEstadoProcesoEntrevistaServiceTest.java
@@ -1,17 +1,36 @@
 package com.entrevistador.orquestador.dominio.service;
 
 import com.entrevistador.orquestador.dominio.excepciones.ActualizarEstadoEntrevistaException;
+import com.entrevistador.orquestador.dominio.excepciones.IdNoEncontradoException;
 import com.entrevistador.orquestador.dominio.model.dto.ProcesoEntrevistaDto;
 import com.entrevistador.orquestador.dominio.model.enums.EstadoProcesoEnum;
 import com.entrevistador.orquestador.dominio.model.enums.FuenteEnum;
+import com.entrevistador.orquestador.dominio.port.ProcesoEntrevistaDao;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ActualizarEstadoProcesoEntrevistaServiceTest {
+    @InjectMocks
+    private ActualizarEstadoProcesoEntrevistaService actualizarEstadoProcesoEntrevistaService;
+    @Mock
+    private ProcesoEntrevistaDao procesoEntrevistaDao;
 
     private ProcesoEntrevistaDto procesoEntrevistaDtoVacio = ProcesoEntrevistaDto.builder()
             .uuid("")
@@ -22,6 +41,7 @@ public class ActualizarEstadoProcesoEntrevistaServiceTest {
             .build();
 
     private ProcesoEntrevistaDto procesoEntrevistaDtoActualizar = ProcesoEntrevistaDto.builder()
+            .uuid("any")
             .fechaYHora(new Date())
             .estado(EstadoProcesoEnum.AC)
             .fuente(FuenteEnum.ANALIZADOR)
@@ -29,14 +49,58 @@ public class ActualizarEstadoProcesoEntrevistaServiceTest {
             .build();
 
     @Test
-    public void dtoVacioActualizarTest() {
+    void dtoVacioActualizarTest() {
         assertThrows(ActualizarEstadoEntrevistaException.class, () ->
                 this.procesoEntrevistaDtoActualizar.actualizar(procesoEntrevistaDtoVacio));
     }
 
     @Test
-    public void dtoActualizarTest() {
+    void dtoActualizarTest() {
         assertTrue(this.procesoEntrevistaDtoVacio.actualizar(procesoEntrevistaDtoActualizar));
     }
 
+    @Test
+    void testEjecutar() {
+        when(this.procesoEntrevistaDao.obtenerEventoPorId(anyString())).thenReturn(Mono.just(procesoEntrevistaDtoVacio));
+        when(this.procesoEntrevistaDao.actualizar(any())).thenReturn(Mono.just(procesoEntrevistaDtoActualizar));
+
+        Mono<Void> publisher = this.actualizarEstadoProcesoEntrevistaService.ejecutar(procesoEntrevistaDtoActualizar);
+
+        StepVerifier
+                .create(publisher)
+                .verifyComplete();
+
+        verify(this.procesoEntrevistaDao, timeout(1)).obtenerEventoPorId(anyString());
+        verify(this.procesoEntrevistaDao, times(1)).actualizar(any());
+    }
+
+    @Test
+    void testEjecutar_WhenIdNoEncontradoException() {
+        when(this.procesoEntrevistaDao.obtenerEventoPorId(anyString())).thenReturn(Mono.empty());
+
+        Mono<Void> publisher = this.actualizarEstadoProcesoEntrevistaService.ejecutar(procesoEntrevistaDtoVacio);
+
+        StepVerifier
+                .create(publisher)
+                .expectError(IdNoEncontradoException.class)
+                .verify();
+
+        verify(this.procesoEntrevistaDao, timeout(1)).obtenerEventoPorId(anyString());
+        verify(this.procesoEntrevistaDao, times(0)).actualizar(any());
+    }
+
+    @Test
+    void testEjecutar_WhenActualizarEstadoEntrevistaException() {
+        when(this.procesoEntrevistaDao.obtenerEventoPorId(anyString())).thenReturn(Mono.just(procesoEntrevistaDtoActualizar));
+
+        Mono<Void> publisher = this.actualizarEstadoProcesoEntrevistaService.ejecutar(procesoEntrevistaDtoVacio);
+
+        StepVerifier
+                .create(publisher)
+                .expectError(ActualizarEstadoEntrevistaException.class)
+                .verify();
+
+        verify(this.procesoEntrevistaDao, timeout(1)).obtenerEventoPorId(anyString());
+        verify(this.procesoEntrevistaDao, times(0)).actualizar(any());
+    }
 }
