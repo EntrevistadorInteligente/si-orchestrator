a6aa1ccae542244c5d5e00192d91a7b8030829cb
kmiiloberrio-dev <kmiiloberrio@gmail.com>
2024-05-27 21:01:36 -0500

    Reformat code

--- src/main/java/com/entrevistador/orquestador/application/service/OrquestadorEntrevistaService.java
+++ src/main/java/com/entrevistador/orquestador/application/service/OrquestadorEntrevistaService.java
@@ -26,7 +26,7 @@ public class OrquestadorEntrevistaService implements OrquestadorEntrevista {
 
     @Override
     public Mono<Void> receptorInformacionEmpresa(String idEntrevista, String idInformacionEmpresaRag) {
-        log.info(String.format("Recibiendo informacion empresa para entervista id : %s",idEntrevista));
+        log.info(String.format("Recibiendo informacion empresa para entervista id : %s", idEntrevista));
         return this.entrevistaDao.actualizarIdInformacionEmpresaRag(idEntrevista, idInformacionEmpresaRag)
                 .then(this.validadorEventosSimultaneosService.ejecutar(idEntrevista))
                 .flatMap(ragsIdsDto -> enviarInformacionEntrevistaAPreparador(ragsIdsDto, idEntrevista));
@@ -35,15 +35,22 @@ public class OrquestadorEntrevistaService implements OrquestadorEntrevista {
 
     @Override
     public Mono<Void> receptorHojaDeVidaMatch(MensajeValidacionMatch mensajeValidacionMatch) {
-        log.info(String.format("Recibiendo informacion validacion hoja de vida para entervista id : %s",mensajeValidacionMatch.getIdEntrevista()));
-        return this.entrevistaDao.actualizarEstadoHojaDeVida(mensajeValidacionMatch.getIdEntrevista(), mensajeValidacionMatch.isMatchValido())
+        log.info(String.format("Recibiendo informacion validacion hoja de vida para entervista id : %s",
+                mensajeValidacionMatch.getIdEntrevista()));
+        return this.entrevistaDao.actualizarEstadoHojaDeVida(
+                        mensajeValidacionMatch.getIdEntrevista(),
+                        mensajeValidacionMatch.isMatchValido()
+                )
                 .then(this.validadorEventosSimultaneosService.ejecutar(mensajeValidacionMatch.getIdEntrevista()))
-                .flatMap(ragsIdsDto -> enviarInformacionEntrevistaAPreparador(ragsIdsDto, mensajeValidacionMatch.getIdEntrevista()));
+                .flatMap(ragsIdsDto -> enviarInformacionEntrevistaAPreparador(
+                        ragsIdsDto,
+                        mensajeValidacionMatch.getIdEntrevista()
+                ));
     }
 
     private Mono<Void> enviarInformacionEntrevistaAPreparador(RagsIdsDto ragsIdsDto, String idEntrevista) {
 
-        if(ragsIdsDto != null){
+        if (ragsIdsDto != null) {
             log.info(String.format("Enviando informacion a preparador para entervista id : %s", idEntrevista));
             return this.jmsPublisherClient.generarEntrevista(SolicitudGeneracionEntrevistaDto.builder()
                             .idEntrevista(idEntrevista)
