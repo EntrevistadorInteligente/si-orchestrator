70265153056a427b3cbbf4b030e1fcc7d0c231e5
DylanSerkovich <n00189659@upn.pe>
2024-06-03 12:11:00 -0500

    Agregar validador de tamaño y optimizar codigo

--- src/main/java/com/entrevistador/orquestador/dominio/model/enums/CodigoErrorEnum.java
+++ src/main/java/com/entrevistador/orquestador/dominio/model/enums/CodigoErrorEnum.java
@@ -8,6 +8,7 @@ public enum CodigoErrorEnum {
     ENTREVISTA_LIMITE_EXCEDIDO("E001", "No se pueden crear más entrevistas. Faltan {0} horas para desbloquear."),
     ENTREVISTA_EN_PROCESO("E002", "El usuario tiene una entrevista en proceso."),
     HOJA_DE_VIDA_INVALIDA("E003", "El documento no es valido"),
+    SIZE_HOJA_DE_VIDA_EXCEDIDO("E004","Tamaño de archivo excedido"),
     INTERNAL_SERVER_ERROR("E500", "Internal Server Error");
 
     @Getter
--- src/main/java/com/entrevistador/orquestador/dominio/service/ValidadorPdfService.java
+++ src/main/java/com/entrevistador/orquestador/dominio/service/ValidadorPdfService.java
@@ -7,10 +7,13 @@ import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
 public class ValidadorPdfService {
 
     private static final byte[] PDF_HEADER = "%PDF-".getBytes(StandardCharsets.US_ASCII);
+    private static final long MAX_FILE_SIZE = 3 * 1024 * 1024; // 3MB en bytes
+    private static final int HEADER_LENGTH = PDF_HEADER.length;
 
     public Mono<byte[]> ejecutar(FilePart file) {
         return DataBufferUtils.join(file.content())
@@ -18,24 +21,22 @@ public class ValidadorPdfService {
                     byte[] bytes = new byte[dataBuffer.readableByteCount()];
                     dataBuffer.read(bytes);
                     DataBufferUtils.release(dataBuffer);
-                    if (esPdf(bytes)) {
-                        return Mono.just(bytes);
-                    } else {
+                    if(!esPdf(bytes)){
                         return Mono.error(new HojaDeVidaInvalidaException(CodigoErrorEnum.HOJA_DE_VIDA_INVALIDA));
                     }
+                    if (bytes.length > MAX_FILE_SIZE) {
+                        return Mono.error(new HojaDeVidaInvalidaException(CodigoErrorEnum.SIZE_HOJA_DE_VIDA_EXCEDIDO));
+                    }
+                    return Mono.just(bytes);
                 });
     }
 
     private boolean esPdf(byte[] bytes) {
-        if (bytes.length < PDF_HEADER.length) {
+        if (bytes.length < HEADER_LENGTH) {
             return false;
         }
-        for (int i = 0; i < PDF_HEADER.length; i++) {
-            if (bytes[i] != PDF_HEADER[i]) {
-                return false;
-            }
-        }
-        return true;
+        return IntStream.range(0,HEADER_LENGTH)
+                .allMatch(i -> bytes[i] == PDF_HEADER[i]);
     }
 
 }
