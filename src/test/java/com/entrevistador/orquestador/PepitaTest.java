a8a3cb93cc6ef2da9ab04f9d4e41566952a260df
kmiiloberrio-dev <kmiiloberrio@gmail.com>
2024-07-08 17:06:30 -0500

    Add pepita test

---
+++ src/test/java/com/entrevistador/orquestador/PepitaTest.java
@@ -0,0 +1,10 @@
+package com.entrevistador.orquestador;
+
+import org.junit.jupiter.api.Test;
+
+public class PepitaTest {
+    @Test
+    void test() {
+        System.out.println("PepitaTest");
+    }
+}
